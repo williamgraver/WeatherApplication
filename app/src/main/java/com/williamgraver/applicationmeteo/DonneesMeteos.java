f247764ef30e1d440ecabd6951cf789465787a3f
WilliamGraver <williamgraver@msn.com>
2019-03-13 14:18:02 +0100

    Ajout données meteo

---
+++ app/src/main/java/com/williamgraver/applicationmeteo/DonneesMeteos.java
@@ -0,0 +1,8 @@
+package com.williamgraver.applicationmeteo;
+
+public class DonneesMeteos {
+    CityInfo cityInfo;
+    CurrentCondition currentCondition;
+    FcstDay fcstDay_0, fcstDay_1, fcstDay_2, fcstDay_3, fcstDay_4;
+    ForecastInfo forecastInfo;
+}
