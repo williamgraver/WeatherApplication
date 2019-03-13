5e0aa110cfacfc6fdd655b855b83c520a2240df1
williamgraver <williamgraver@msn.com>
2019-03-13 12:46:22 +0100

    Geolocalisation terminée

--- app/src/main/java/com/williamgraver/applicationmeteo/MainActivity.java
+++ app/src/main/java/com/williamgraver/applicationmeteo/MainActivity.java
@@ -52,32 +52,28 @@ public class MainActivity extends AppCompatActivity {
 
         Intent t = getIntent();
         account = t.getParcelableExtra("GoogleAccount");
-        callWebService(null, null);
+
         configureNavHeader(account, getIntent().getStringExtra("UserName"));
 
         // Geolocalisation
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        fusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            // Logic to handle location object
-                            callWebService((Double.toString(location.getLatitude())), Double.toString(location.getLongitude()));
+            callWebService(null, null);
+        } else {
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                // Logic to handle location object
+                                callWebService((Double.toString(location.getLongitude())), Double.toString(location.getLatitude()));
+                            }
                         }
-                    }
-                });
+                    });
+        }
+
+
     }
 
 
@@ -124,7 +120,6 @@ public class MainActivity extends AppCompatActivity {
         String url = "";
         if ((longitude != null && longitude != null ) && (!longitude.isEmpty() && !longitude.isEmpty())){
             url = "https://www.prevision-meteo.ch/services/json/lat="+latitude + "lng=" + longitude ;
-            System.out.print("URL DE LA REQUETE : " + url);
 
         } else {
             url= "https://www.prevision-meteo.ch/services/json/grenoble";
